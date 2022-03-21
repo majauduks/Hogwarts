9830057a2fbe8a58989504ab22593bf12fa4e025
majauduks <93546860+majauduks@users.noreply.github.com>
2022-03-21 14:26:34 +0100

    Add files via upload

---
+++ SEDC.Oop.Homeworks.Classes/SEDC.Oop.Homeworks.Classes.Hogwarts/HogwartsCreatures/Creatures.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Oop.Homeworks.Classes.Hogwarts.Creatures
+{
+    public class TheCreatures
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int PowerLevel { get; set; }
+        public bool IsTamed { get; set; }
+
+    public TheCreatures (string name,int powerLevel, int age, bool isTamed)
+    {
+        Name = name;
+        Age = age;
+        IsTamed = isTamed;
+        PowerLevel = powerLevel;
+    }
+
+
+    }
+
+
+}
